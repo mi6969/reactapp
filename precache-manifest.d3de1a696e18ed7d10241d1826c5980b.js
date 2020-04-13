94e0400ca0e567133f6ad7c4de78ee14cb7c639c
Abdullah Al Mamun <mamunvai76@gmail.com>
2020-04-13 20:12:05 +0600

    Updates

---
+++ precache-manifest.d3de1a696e18ed7d10241d1826c5980b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240933a7775a2b2b8b9db9708db57958",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "551ba05d9917a1970d0a",
+    "url": "/reactapp/static/css/main.d4cb4be8.chunk.css"
+  },
+  {
+    "revision": "f78e1d251a40af6d532b",
+    "url": "/reactapp/static/js/2.814be438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.814be438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551ba05d9917a1970d0a",
+    "url": "/reactapp/static/js/main.26defe59.chunk.js"
+  },
+  {
+    "revision": "8c7bbda30b317e7a7207",
+    "url": "/reactapp/static/js/runtime-main.15d89ed6.js"
+  }
+]);
